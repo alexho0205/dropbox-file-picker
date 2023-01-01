c2200bf2b3201bd63b07d5046997572bad090d0c
alexho0205 <96756700+alexho0205@users.noreply.github.com>
2023-01-01 10:13:02 +0800

    初始化專案

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    //handle incomming requests here..
+
+});
+var fs = require('fs');
+
+//server.listen(5000); //3 - listen for any incoming requests
+
+//
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(5000);
+});
+
+console.log('Node.js web server at port 5000 is running..')
